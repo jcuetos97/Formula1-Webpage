f25b57aa4f5dc7f14495a364a2cd2c3aacd234be
Jorge Besnier <jorgebesnierb@outlook.com>
2022-07-28 21:11:20 -0500

    Base api calls to get information about races, and list of pilots for a selected race.

--- Assets/js/scriptF1.js
+++ Assets/js/scriptF1.js
@@ -2,6 +2,7 @@
 // Variable declaraitons
 // ######################################################################
 var arrFormulaCircuits = [];
+var arrFormulaPilots = [];
 // ######################################################################
 // STEP !: Get all races from a determine year.
 // ######################################################################
@@ -28,31 +29,49 @@ fetch(szUrlFormula1, options)
   })
   .then(function (data) {
     arrFormulaCircuits = [];
-    let race = {
-        nRaceYearId: '',
-        szRaceName: ''
-      };
+    // JBE:Uncomment to debug reponse from api
     // console.log("aqui", data.response)
     // console.log("aqui", data.response[1].circuit.id)
-    // console.log('Formula 1 Repo Issues \n----------');
     for (var i = 0; i < data.response.length; i++) {
-    //   console.log("aqui", data.response[i].id)
-    //   console.log("aqui", data.response[i].circuit.name)
-    //   console.log("aqui", data.response[i].status)
       if (data.response[i].status === "Completed") {
-        let race = {
-            nRaceYearId: '',
-            szRaceName: ''
-          };
-        race.nRaceYearId =  data.response[i].id;
-        race.szRaceName = data.response[i].circuit.name;
-        console.log("race",race);
-        console.log("race",race.nRaceYearId);
-        console.log("race",race.szRaceName);
-        arrFormulaCircuits.push(race);
-        // arrFormulaCircuits.push({nRaceYearId:data.response[i].id, szRaceName:data.response[i].circuit.name});
+        arrFormulaCircuits.push({nRaceYearId:data.response[i].id, szRaceName:data.response[i].circuit.name});
       }
     }
     console.log('Formula 1 Repo Issues \n----------', arrFormulaCircuits);
   })
   .catch(err => console.error(err));
+
+
+
+// ######################################################################
+// STEP !: Get rankings from a race to get all pilots
+// ######################################################################
+//Get year selected by the user.
+var nRaceId = 1508
+
+// JBE: Build the url to get a certain year of racing.
+var szUrlFormula1Rankings = 'https://api-formula-1.p.rapidapi.com/rankings/races?race='+ nRaceId;
+
+const options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6ce45788bamshb1899499874cc3bp1c8ec3jsn1441744eec4b',
+		'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+	}
+};
+
+fetch(szUrlFormula1Rankings, options2)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    arrFormulaPilots = [];
+    for (var i = 0; i < data.response.length; i++) {
+        arrFormulaPilots.push({nDriverId:data.response[i].driver.id, szDriverName:data.response[i].driver.name});
+    }
+    console.log('Formula 1 Repo Issues \n----------', arrFormulaPilots);
+  })
+  .catch(err => console.error(err));
+
+
+

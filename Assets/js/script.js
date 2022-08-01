71fa4804ca549bf66ee4d81cde5cbbc15d4a4f40
David Torres <davidtorresc8@gmail.com>
2022-07-31 19:01:19 -0500

    Created user selection to generate comparison

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,6 +12,7 @@ let nYearArray = [];
 let nYear = null;
 var arrFormulaCircuits = [];
 var arrFormulaPilots = [];
+var arrPilotsComparison = [];
 
 //Navbar Burger - Mobile
 document.addEventListener("DOMContentLoaded", () => {
@@ -76,13 +77,14 @@ function getRacecNames(nYearArray) {
 
 //Put Career names in the HTML Dropdown
 function putCareerNames(arrFormulaCircuits) {
-  document.querySelector("option[value=change-text-race]").text = "Ready to search Race";
+  document.querySelector("option[value=change-text-race]").text =
+    "Ready to search Race";
   console.log(arrFormulaCircuits);
-  for (let i=0; i<arrFormulaCircuits.length; i++) {
+  for (let i = 0; i < arrFormulaCircuits.length; i++) {
     let dropDownValue = document.getElementById("select_race_name");
     let option = document.createElement("option");
     option.value = arrFormulaCircuits[i].szRaceName;
-    option.text = arrFormulaCircuits[i].szRaceName;;
+    option.text = arrFormulaCircuits[i].szRaceName;
     dropDownValue.add(option);
   }
 }
@@ -91,10 +93,9 @@ function putCareerNames(arrFormulaCircuits) {
 function onSelectingCareerName() {
   let dropDownValue = document.getElementById("select_race_name").value;
   let raceName = dropDownValue;
-  console.log(raceName)
-  var IDraceName = arrFormulaCircuits.find(o => o.szRaceName === raceName)
-  console.log(IDraceName.nRaceYearId)
-  callPilotAPI(IDraceName.nRaceYearId)
+  console.log(raceName);
+  var IDraceName = arrFormulaCircuits.find((o) => o.szRaceName === raceName);
+  callPilotAPI(IDraceName.nRaceYearId);
 }
 
 //Get the Pilot values based on the race from the API
@@ -103,7 +104,6 @@ function callPilotAPI(IDraceName) {
   var szUrlFormula1Rankings =
     "https://api-formula-1.p.rapidapi.com/rankings/races?race=" + IDraceName;
 
-  
   //TODO: Check with Jorge if we can delete this, we already have these values in the first lines of codes
   const options2 = {
     method: "GET",
@@ -125,30 +125,133 @@ function callPilotAPI(IDraceName) {
           szDriverName: data.response[i].driver.name,
         });
       }
-      putPilotNames(arrFormulaPilots)
       console.log("Formula 1 Repo Issues \n----------", arrFormulaPilots);
+      putPilotNames(arrFormulaPilots);
     })
     .catch((err) => console.error(err));
 }
 
 //Put Pilot names in the HTML Dropdown
 function putPilotNames(arrFormulaPilots) {
-  document.querySelector("option[value=change-text-pilot1]").text = "Ready to search Pilot"
-  for (let i=0; i<arrFormulaPilots.length; i++) {
+  document.querySelector("option[value=change-text-pilot1]").text =
+    "Ready to search Pilot";
+  for (let i = 0; i < arrFormulaPilots.length; i++) {
     let dropDownValue = document.getElementById("select_pilot_name1");
     let option = document.createElement("option");
     option.value = arrFormulaPilots[i].szDriverName;
-    option.text = arrFormulaPilots[i].szDriverName;;
+    option.text = arrFormulaPilots[i].szDriverName;
     dropDownValue.add(option);
   }
-  document.querySelector("option[value=change-text-pilot2]").text = "Ready to search Pilot"
-  for (let i=0; i<arrFormulaPilots.length; i++) {
+  document.querySelector("option[value=change-text-pilot2]").text =
+    "Ready to search Pilot";
+  for (let i = 0; i < arrFormulaPilots.length; i++) {
     let dropDownValue = document.getElementById("select_pilot_name2");
     let option = document.createElement("option");
     option.value = arrFormulaPilots[i].szDriverName;
-    option.text = arrFormulaPilots[i].szDriverName;;
+    option.text = arrFormulaPilots[i].szDriverName;
     dropDownValue.add(option);
   }
+}
+
+//Select Pilot from Dropdown
+function onSelectingPilot1() {
+  //Selecting pilot1
+  let dropDownValuePilot1 = document.getElementById("select_pilot_name1").value;
+  let pilotName1 = dropDownValuePilot1;
+  console.log(pilotName1);
+  arrPilotsComparison.push(pilotName1);
+}
+
+function onSelectingPilot2() {
+  //Selecting Pilot2
+  let dropDownValuePilot2 = document.getElementById("select_pilot_name2").value;
+  let pilotName2 = dropDownValuePilot2;
+  console.log(pilotName2);
+  arrPilotsComparison.push(pilotName2);
+  userFinalAnswer()
+}
+
+//Ask the user if he/she wants to run the comparison
+function userFinalAnswer() {
+  const userFinalAnswerSection = document.getElementById("user-final-answer");
+  const h2Title = document.createElement("h2");
+  //Giving it a class of "subtitle"
+  h2Title.classList.add("subtitle");
+  //Creating text for h2Title
+  const h2Text = document.createTextNode("Would you like to start the comparison?");
+  //Apending h2Text to h2Title
+  h2Title.appendChild(h2Text);
+  //Apending h2Title to userFinalAnswerSection
+  userFinalAnswerSection.append(h2Title);
+  //Create button
+  const button = document.createElement("button");
+  //Giving it a class of "button"
+  button.classList.add("button");
+  button.classList.add("my-button");
+  button.textContent = "Run Comparison"
+  //giving it color background
+  button.style.backgroundColor = "#e25a28";
+  userFinalAnswerSection.append(button);
+  button.onclick = function() {createComparisonDashboard()};
+}
+
+//-------- Pilot Section --------
+function createComparisonDashboard() {
+  const pilot1 = arrPilotsComparison[0];
+  const pilot2 = arrPilotsComparison[1];
+  console.log(pilot1)
+  console.log(pilot2)
+  //Grab pilot-section by ID
+  const pilotSection = document.getElementById("pilot-section");
+  //Create an H1 element
+  //------------ H1 Section --------
+  const h1Title = document.createElement("h1");
+  //Giving it a class of "title" and "h1"
+  h1Title.classList.add("title");
+  h1Title.classList.add("h1");
+  //Creating text for h1Title
+  const h1Text = document.createTextNode("Pilot Comparison");
+  //Apending h1Text to h1Title
+  h1Title.appendChild(h1Text);
+  //Apending h1Title to pilotSection
+  pilotSection.append(h1Title);
+  //-------- COMPARATIVE SQUARE --------
+  //Creating father div (BULMA NEEDS)
+  const divComparativeSectionFather = document.createElement("div");
+  //giving classes to that div father
+  divComparativeSectionFather.classList.add("container");
+  divComparativeSectionFather.classList.add("is-max-desktop");
+  //apending that div to the pilot-section
+  pilotSection.append(divComparativeSectionFather);
+  //Creating child div (BULMA NEEDS)
+  const divComparativeSectionChild = document.createElement("div");
+  //giving classes to that div child
+  divComparativeSectionChild.classList.add("notification");
+  divComparativeSectionChild.classList.add("is-primary");
+  divComparativeSectionChild.style.backgroundColor = "#e25a28";
+  //apending that div to the pilot-section
+  pilotSection.append(divComparativeSectionChild);
+  //-------- H2 Section --------
+  //Creating Pilot 1 Section using h2 element
+  const pilot1h2 = document.createElement("h2");
+  //giving pilot1h2 a class of subtitle
+  pilot1h2.classList.add("subtitle");
+  //creating text for pilot1
+  const textPilot1 = document.createTextNode(`${pilot1}`);
+  //appending text to pitlot1h2
+  pilot1h2.appendChild(textPilot1);
+  //appending pilot to pilotSection
+  divComparativeSectionChild.append(pilot1h2);
+  //Creating Pilot 2 Section using h2 element
+  const pilot2h2 = document.createElement("h2");
+  //giving pilot1h2 a class of subtitle
+  pilot2h2.classList.add("subtitle");
+  //creating text for pilot1
+  const textPilot2 = document.createTextNode(`${pilot2}`);
+  //appending text to pitlot1h2
+  pilot2h2.appendChild(textPilot2);
+  //appending pilot to pilotSection
+  divComparativeSectionChild.append(pilot2h2);
 }
 
 //TODO: Make some global variables to save Pilot Names and Pilot Race Name,
